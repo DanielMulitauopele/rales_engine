0bf64ddd5c43ab5edfcbbf6012a1d3146b89a059
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2018-10-03 15:26:19 -0600

    adds customers controller and index method, route for customers index

--- spec/factories/customers.rb
+++ spec/factories/customers.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :customer do
-    first_name { "MyText" }
-    last_name { "MyText" }
+    first_name { "Aang" }
+    last_name { "Of the Southern Air Temple" }
   end
 end
